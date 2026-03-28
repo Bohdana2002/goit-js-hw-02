6cd6713745095c8699db43a0fe02c937c396e6ac
Bohdana Turcheniak <bogdanaturcheniak@gmail.com>
2026-03-28 13:58:11 +0200

    final-task-done

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,16 @@
 function checkForSpam(message) {
-    const forbiddenWordOne = 'spam';
-    const forbiddenWordTwo = 'sale';
-    message = message.toLowerCase();
-    return message.includes(forbiddenWordOne) || message.includes(forbiddenWordTwo);
+  const forbiddenWordOne = 'spam';
+  const forbiddenWordTwo = 'sale';
+  message = message.toLowerCase();
+  return (
+    message.includes(forbiddenWordOne) || message.includes(forbiddenWordTwo)
+  );
 }
 
-console.log(checkForSpam("Latest technology news")); 
-console.log(checkForSpam("JavaScript weekly newsletter")); 
-console.log(checkForSpam("Get best sale offers now!")); 
-console.log(checkForSpam("Amazing SalE, only tonight!")); 
-console.log(checkForSpam("Trust me, this is not a spam message")); 
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
